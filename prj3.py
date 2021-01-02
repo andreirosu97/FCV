70a0639dae038d273b62b958dba4d9cdab4dd714
Roşu Andrei <andreirosu97@gmail.com>
2021-01-02 21:55:10 +0200

    working code, todo doc

--- prj3.py
+++ prj3.py
@@ -1,69 +1,76 @@
 import numpy as np
-import cv2 as cv
-from tkinter import *
-from tkinter import filedialog
+import cv2
 import os
 import json 
-from matplotlib import pyplot as plt
+from tkinter import *
+from tkinter import filedialog
 
-cap = cv.VideoCapture(1)
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+root = Tk()
+filename = filedialog.askopenfilename()
 
-out = cv.VideoWriter('outpy.avi',cv.VideoWriter_fourcc('M','J','P','G'), 20, (width,height))
+img = cv2.imread(filename)
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-i = 0
-ret, frame = cap.read()
-    # if frame is read correctly ret is True
-if not ret:
-    print("Can't receive frame (stream end?). Exiting ...")
-    exit(1)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-previous_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-previous_frame = cv.GaussianBlur(previous_frame,(17,17),0)
-i = 0
-while True:
-    ret, frame = cap.read()
-    #skip one frame
-    if (i==0):
-        i = 1
-        continue
-    else:
-        i = 0
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (152, 1))
+dilate = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel)
 
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+cntrs = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-    #getting the frame
-    color_frame = frame.copy()
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+result = img.copy()
+lineCount = 1
+for c in cntrs:
+    box = cv2.boundingRect(c)
+    x,y,w,h = box
+    imageH = img.shape[0]
+    if h > imageH*0.01 :
+        cv2.rectangle(result, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        line = img[y:y+h, x:x+w]
+        cv2.imwrite("line_" + str(lineCount) + ".png", line)
+        lineCount+=1
 
-    #frame alterations
-    frame = cv.GaussianBlur(frame,(17,17),0) #removes noise
+cv2.imwrite("line_extraction_thresholding.png", thresh)
+cv2.imwrite("line_extraction_dilate.png", dilate)
+cv2.imwrite("line_extraction_result.png", result)
 
-    #thresholding
-    frame_diff = cv.absdiff(frame, previous_frame)
-    ret,threshold = cv.threshold(frame_diff,25,255,cv.THRESH_BINARY)
+cv2.imshow("RESULT", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-    avg = np.average(threshold)
-    if (avg <= 0):
-        cv.line(color_frame,(0,0),(width, height),(0,0,255),5)
-        cv.line(color_frame,(width,0),(0, height),(0,0,255),5)
-    else:
-        out.write(color_frame)
+for i in range(1,lineCount):
+    img_line = cv2.imread("line_" + str(i) + ".png")
 
-    previous_frame = frame.copy()
-    cv.imshow('frame', color_frame)
-    cv.imshow('threshold', threshold)
+    gray = cv2.cvtColor(img_line, cv2.COLOR_BGR2GRAY)
+    
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    if cv.waitKey(1) == ord('q'):
-        break
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
+    erode = cv2.erode(thresh, kernel, iterations=1)
 
-cap.release()
-out.release()
-cv.destroyAllWindows()
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT , (1,10))
+    dilate = cv2.morphologyEx(erode, cv2.MORPH_DILATE, kernel)
+
+    cntrs = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    result = img_line.copy()
+    for c in cntrs:
+        area = cv2.contourArea(c)
+        if area > 200:
+            box = cv2.boundingRect(c)
+            x,y,w,h = box
+            cv2.rectangle(result, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        elif area > 50:
+            box = cv2.boundingRect(c)
+            x,y,w,h = box
+            cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+    cv2.imshow("gray", gray)
+    cv2.imshow("erode", erode)
+    cv2.imshow("thresh", thresh)
+    cv2.imshow("dilate", dilate)
+    cv2.imshow("RESULT", result)
+    cv2.imwrite("line_" + str(i) + "_character_extraction_result.png", result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
